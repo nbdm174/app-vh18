30503d249b9dc78469315c06a83aed4a2b23f36e
nbdm174 <nguyenbaducmanh174@gmail.com>
2026-03-16 15:08:50 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ function App() {
     <>
     <img src='image.png' width={560} height={300}/>
     <h1>BigBang</h1>
+    <table border={1}>
+      <tr>
+        <th>STT</th>
+        <th>Ma sinh vien</th>
+        <th>Ho va ten</th>
+        <th>Diem</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>B23DVCN100</td>
+        <td>To An An</td>
+        <td>7</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>B23DVCN101</td>
+        <td>To An A</td>
+        <td>9</td>
+      </tr>
+    </table>
     </>
   );
 }
